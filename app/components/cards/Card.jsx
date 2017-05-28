7ed1c3e2d3a10aafb7bbc600e4ee067039cc38bf
JSRossiter <jeffrey.rossiter@me.com>
2017-05-28 11:47:33 -0700

    change hover method to drop, cards can be repositioned functionally

--- app/components/cards/Card.jsx
+++ app/components/cards/Card.jsx
@@ -18,53 +18,29 @@ const cardSource = {
     };
   }
 };
-
 const cardTarget = {
-  hover(props, monitor, component) {
+  drop(props, monitor, component) {
     const dragIndex = monitor.getItem().index;
     const hoverIndex = props.index;
-    console.log('dragIndex', dragIndex, '\nhoverIndex', hoverIndex, '\npropsId', props.id);
     // Don't replace items with themselves
     if (dragIndex === hoverIndex) {
       return;
     }
 
-    // Determine rectangle on screen
-    const hoverBoundingRect = findDOMNode(component).getBoundingClientRect();
+    // const hoverBoundingRect = findDOMNode(component).getBoundingClientRect();
+    // const hoverMiddleY = (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
+    // const clientOffset = monitor.getClientOffset();
+    // const hoverClientY = clientOffset.y - hoverBoundingRect.top;
 
-    // Get vertical middle
-    const hoverMiddleY = (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
-
-    // Determine mouse position
-    const clientOffset = monitor.getClientOffset();
-
-    // Get pixels to the top
-    const hoverClientY = clientOffset.y - hoverBoundingRect.top;
-
-    // Only perform the move when the mouse has crossed half of the items height
-    // When dragging downwards, only move when the cursor is below 50%
-    // When dragging upwards, only move when the cursor is above 50%
-
-    // Dragging downwards
     // if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
     //   return;
     // }
-
-    // // Dragging upwards
     // if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
     //   return;
     // }
 
-    // Time to actually perform the action
-    setTimeout(() => {
-      props.moveCard(dragIndex, hoverIndex);
-    }, 100);
-
-    // Note: we're mutating the monitor item here!
-    // Generally it's better to avoid mutations,
-    // but it's good here for the sake of performance
-    // to avoid expensive index searches.
-    monitor.getItem().index = hoverIndex;
+    props.moveCard(dragIndex, hoverIndex);
+    // monitor.getItem().index = hoverIndex;
   }
 };
 
